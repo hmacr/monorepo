1a92bd0d50243be061552f410ff95bfa5974283f
Hemachandar <hmac.devo@gmail.com>
2025-10-14 02:22:24 +0530

    edit 9

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
             Get started by editing <code>apps/web/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>Edit 8</li>
+          <li>Edit 9</li>
         </ol>
 
         <div className={styles.ctas}>
